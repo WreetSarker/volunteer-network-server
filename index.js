c1c692e506b32663f4db78aefd094b39d7febadb
WreetSarker <wreet.sarker@gmail.com>
2021-03-29 10:35:45 +0600

    added express and mongodb connection

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+require('dotenv').config();
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.enmmk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const eventCollection = client.db("volunteer").collection("events");
+    console.log('database connected successfully');
+
+});
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from server')
+})
+
+app.listen(process.env.PORT || 4000);
